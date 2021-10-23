5c17add261b597827951961399010f82db9e07fa
Hiroshi Urabe <mail@torounit.com>
2021-10-24 00:03:53 +0900

    backend

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ```bash
 cd frontend
+cargo install cargo-watch
 cargo install trunk wasm-bindgen-cli
 rustup target add wasm32-unknown-unknown 
 trunk serve 
+
 ```
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World???");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
 }
