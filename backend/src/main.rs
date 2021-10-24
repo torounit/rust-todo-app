6864bc6b3a71e8187f48ebb42a201b2be1967d12
Hiroshi Urabe <mail@torounit.com>
2021-10-24 17:45:09 +0900

    backend todo api

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use diesel::prelude::*;
 use todo_app_backend::models::Todo;
 use todo_app_backend::schema::todos::dsl::todos;
@@ -10,7 +10,11 @@ async fn index(_req: HttpRequest) -> impl Responder {
     let results = todos
         .load::<Todo>(&connection)
         .expect("Error loading todos");
-    web::Json(results)
+    //    web::Json(results)
+    let body = serde_json::to_string(&results).unwrap();
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(body)
 }
 
 #[actix_web::main]
