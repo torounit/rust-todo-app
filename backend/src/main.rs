2e051313517ad532977b3198858fc54d905ec12f
Hiroshi Urabe <mail@torounit.com>
2021-10-24 22:08:08 +0900

    request

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_cors::Cors;
+use actix_web::http::header;
+use actix_web::{get, http, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use diesel::prelude::*;
 use todo_app_backend::models::Todo;
 use todo_app_backend::schema::todos::dsl::todos;
@@ -19,8 +21,17 @@ async fn index(_req: HttpRequest) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8081))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin_fn(|origin, _req_head| true)
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_header(header::CONTENT_TYPE)
+            .supports_credentials()
+            .max_age(3600);
+        App::new().wrap(cors).service(index)
+    })
+    .bind(("127.0.0.1", 8081))?
+    .run()
+    .await
 }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,14 +1,38 @@
-use yew::prelude::*;
+use serde::Deserialize;
+use yew::{
+    format::{Json, Nothing},
+    prelude::*,
+    services::fetch::{FetchService, FetchTask, Request, Response},
+};
+use Msg::*;
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Todo {
+    pub id: i32,
+    pub content: String,
+}
 
 enum Msg {
-    AddOne,
+    GetTodos,
+    ReceiveResponse(Result<Vec<Todo>, anyhow::Error>),
 }
 
 struct Model {
     // `ComponentLink` is like a reference to a component.
     // It can be used to send messages to the component
     link: ComponentLink<Self>,
-    value: i64,
+    fetch_task: Option<FetchTask>,
+    todos: Vec<Todo>,
+}
+
+impl Model {
+    fn view_todo(&self, (id, todo): (usize, &Todo)) -> Html {
+        return html! {
+            <li>
+                {id}{ ":" }{&todo.content}
+            </li>
+        };
+    }
 }
 
 impl Component for Model {
@@ -16,33 +40,60 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, value: 0 }
+        Self {
+            link,
+            fetch_task: None,
+            todos: vec![],
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
+            ReceiveResponse(response) => {
+                let todos = response.unwrap();
+                self.todos = todos;
+                self.fetch_task = None;
+                true
+            }
+            GetTodos => {
+                let request = Request::get("http://localhost:8081")
+                    .body(Nothing)
+                    .expect("Could not build that request");
+
+                let callback = self.link.callback(
+                    |response: Response<Json<Result<Vec<Todo>, anyhow::Error>>>| {
+                        let Json(data) = response.into_body();
+                        ReceiveResponse(data)
+                    },
+                );
+                // 3. pass the request and callback to the fetch service
+                let task = FetchService::fetch(request, callback).expect("failed to start request");
+                // 4. store the task so it isn't canceled immediately
+                self.fetch_task = Some(task);
+                // we want to redraw so that the page displays a 'fetching...' message to the user
+                // so return 'true'
                 true
             }
         }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
         false
     }
 
     fn view(&self) -> Html {
-        html! {
+        return html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
+                <ul>
+                    { for self.todos.iter().enumerate().map(|e| self.view_todo(e)) }
+                </ul>
             </div>
+        };
+    }
+
+    fn rendered(&mut self, _first_render: bool) {
+        if _first_render {
+            self.update(GetTodos);
         }
     }
 }
